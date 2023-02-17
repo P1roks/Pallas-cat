9e7405450b0497432aa6974e41e4262529377c0d
P1roks <piotrekjakobczyk1@gmail.com>
2023-02-17 10:03:07 +0100

    added font awesome and began working on searchbar

--- frontend/src/Bar.tsx
+++ frontend/src/Bar.tsx
@@ -1,12 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import "./bar.scss"
+import {useState} from 'react';
+
 export const Bar = () => {
 
     return (
-        <div id="bar">
-            <img src="vite.svg" alt="logo" />
-        </div>
+        <nav id="bar">
+            <div id="container">
+                <span id="logo">
+                    <img src="logo.svg" alt="logo" />
+                    <b>Pallas Cat</b>
+                </span>
+                <Search />
+            </div>
+        </nav>
     )
 }
 
 const Search = () => {
+    const [opacity,setOpacity] = useState(0);
 
+    return (
+        <span id="search">
+            <button id="button_search" onClick={(e) => opacity === 0 ? setOpacity(100) : setOpacity(0)}>
+                <FontAwesomeIcon icon={faMagnifyingGlass} size="xl" />
+            </button>
+            <form action="#" id="form_search" method="POST" style={{opacity: opacity}}>
+                <input type="text" name="input_search" id="input_search"
+                placeholder="Wpisz swoje wyszukiwanie..."/>
+                <select name="select_search" id="select_search">
+                    <option value="">CDA</option>
+                    <option value="">Zaluknij</option>
+                </select>
+            </form>
+        </span>
+    )
 }
