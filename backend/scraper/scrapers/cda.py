3cecfc2a098c1640f7659a0f89dcafba22491896
M4rceleq <maciaszczyk.marcel@gmail.com>
2023-02-18 21:31:15 +0100

    add video_id check in cda

--- backend/scraper/scrapers/cda.py
+++ backend/scraper/scrapers/cda.py
@@ -5,22 +5,26 @@ import json
 def get_stream_url(video_id):
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'
     headers = {'User-Agent': user_agent}
-
     res = requests.get(f"https://ebd.cda.pl/620x395/{video_id}", headers=headers)
 
     soup = BeautifulSoup(res.text, 'html.parser')
     div = soup.select('div[player_data]')
-    playerData = json.loads(div[0]['player_data'])
-    highestFormat = list(playerData['video']['qualities'].values())[-1]
+
+    # check whether the video_id points to a website that doesn't contain the actual video
+    if len(div) == 0: return None
+
+    raw_player_data = div[0]['player_data']
+    player_data = json.loads(raw_player_data) if type(raw_player_data) is str else json.loads(raw_player_data[0])
+    highest_format = list(player_data['video']['qualities'].values())[-1]
         
     body = {
         "jsonrpc": "2.0",
         "method": "videoGetLink",
         "params": [
-            playerData['video']['id'],
-            highestFormat,
-            playerData['video']['ts'],
-            playerData['video']['hash2'],
+            player_data['video']['id'],
+            highest_format,
+            player_data['video']['ts'],
+            player_data['video']['hash2'],
             {}
         ], 
         "id": 1
@@ -28,8 +32,8 @@ def get_stream_url(video_id):
 
     headers['Content-Type'] = 'application/json'
     res1 = requests.post("https://www.cda.pl/", data=json.dumps(body), headers=headers)
-    x = json.loads(res1.text)
-    return x['result']['resp']
+    data = json.loads(res1.text)
+    return data['result']['resp']
 
 def search_videos(query):
     return None
