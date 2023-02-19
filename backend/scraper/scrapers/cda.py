5b9b80bd0eb82e45c9b96888a979edcba1078c0d
M4rceleq <maciaszczyk.marcel@gmail.com>
2023-02-19 23:31:23 +0100

    add cda search

--- backend/scraper/scrapers/cda.py
+++ backend/scraper/scrapers/cda.py
@@ -13,12 +13,12 @@ def get_stream_url(video_id: str, full_url = False) -> str | None:
     res = requests.get(get_url, headers=headers)
 
     soup = BeautifulSoup(res.text, 'html.parser')
-    div = soup.select('div[player_data]')
+    div = soup.select_one('div[player_data]')
 
     # check whether the video_id points to a website that doesn't contain the actual video
-    if len(div) == 0: return None
+    if div is None: return None
 
-    raw_player_data = div[0]['player_data']
+    raw_player_data = div['player_data']
     player_data = json.loads(raw_player_data) if type(raw_player_data) is str else json.loads(raw_player_data[0])
     highest_format = list(player_data['video']['qualities'].values())[-1]
         
@@ -41,4 +41,41 @@ def get_stream_url(video_id: str, full_url = False) -> str | None:
     return data['result']['resp']
 
 def search_videos(query):
-    return None
+    res: requests.Response
+
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'
+    headers = {'User-Agent': user_agent}
+    res = requests.get(f'https://www.cda.pl/info/{query}', headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    links = list()
+    videos_clips = soup.select('.video-clip')
+    for video_clip in videos_clips:
+        premium_tag = video_clip.select_one('.flag-video-premium')
+        if premium_tag is not None:
+            continue
+
+        title_tag = video_clip.select_one('.link-title-visit')
+        if title_tag is None:
+            continue
+        title = title_tag.get_text() 
+
+        link_tag = video_clip.select_one('.video-clip-link')
+        if link_tag is None:
+            continue
+
+        link = link_tag['href']
+
+        cover_tag = video_clip.select_one('.video-clip-image')
+        if cover_tag is None:
+            continue
+
+        cover = cover_tag['src']
+
+        links.append({ 
+            'link': link, 
+            'title': title, 
+            'cover': cover 
+        })
+
+    return json.dumps(links)
