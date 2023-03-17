b085c8ab2a2badf2847f80529388e0101387e66a
P1roks <piotrekjakobczyk1@gmail.com>
2023-03-17 10:15:23 +0100

    small changes

--- frontend/src/atoms.ts
+++ frontend/src/atoms.ts
@@ -1,6 +1,6 @@
 import {atom} from "recoil";
 import { getDefaultTheme } from "./utils";
 
-export const lastWatchedTitle = atom({key: "lastWatchedTitle", default: "title"});
+export const lastWatchedTitle = atom({key: "lastWatchedTitle", default: localStorage.getItem('lastTitle') ?? ""});
 export const logRegErr = atom({key: "logRegErr", default: {msg: "", color: "red"}});
 export const theme = atom({key: "theme", default: getDefaultTheme()});
